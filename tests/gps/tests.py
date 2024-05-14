bc0800b215268bb51ebe63f0e9708323a5e13746
François Freitag <mail@franek.fr>
2024-05-14 10:28:30 +0200

    Force job seekers to fill their profile
    The first_name, last_name, and address are required fields.

--- tests/gps/tests.py
+++ tests/gps/tests.py
@@ -7,22 +7,32 @@ from itou.gps.models import FollowUpGroup, FollowUpGroupMembership
 from itou.users.enums import UserKind
 from itou.users.models import User
 from tests.gps.factories import FollowUpGroupFactory
-from tests.users.factories import JobSeekerFactory, PrescriberFactory
+from tests.users.factories import (
+    JobSeekerFactory,
+    JobSeekerWithAddressFactory,
+    PrescriberFactory,
+)
 from tests.utils.test import parse_response_to_soup
 
 
 def test_user_autocomplete():
     member = PrescriberFactory(first_name="gps member first_name")
     beneficiary = JobSeekerFactory(first_name="gps beneficiary first_name")
-    another_beneficiary = JobSeekerFactory(first_name="gps another beneficiary first_name")
+    another_beneficiary = JobSeekerFactory(
+        first_name="gps another beneficiary first_name"
+    )
 
-    FollowUpGroupFactory(beneficiary=beneficiary, memberships=4, memberships__member=member)
+    FollowUpGroupFactory(
+        beneficiary=beneficiary, memberships=4, memberships__member=member
+    )
     FollowUpGroupFactory(beneficiary=another_beneficiary, memberships=2)
 
     # Default to kind=UserKind.JOB_SEEKER
     assert User.objects.autocomplete("gps").count() == 2
     assert User.objects.autocomplete("gps member").count() == 0
-    assert User.objects.autocomplete("gps member", kind=UserKind.PRESCRIBER).count() == 1
+    assert (
+        User.objects.autocomplete("gps member", kind=UserKind.PRESCRIBER).count() == 1
+    )
 
     # We should not get ourself nor the other user because we are a member of his group
     users = User.objects.autocomplete("gps", current_user=member)
@@ -62,7 +72,9 @@ def test_join_group_as_job_seeker(is_referent, client, snapshot):
     follow_up_group = FollowUpGroup.objects.get(beneficiary=job_seeker)
     assert FollowUpGroupMembership.objects.count() == 1
     membership = (
-        FollowUpGroupMembership.objects.filter(member=prescriber).filter(follow_up_group=follow_up_group).first()
+        FollowUpGroupMembership.objects.filter(member=prescriber)
+        .filter(follow_up_group=follow_up_group)
+        .first()
     )
 
     assert membership.is_referent == is_referent
@@ -111,11 +123,19 @@ def test_join_group_as_prescriber(client):
 
 def test_navigation(snapshot, client):
     member = PrescriberFactory(first_name="gps member first_name")
-    member_first_beneficiary = JobSeekerFactory(first_name="gps first beneficiary first_name")
-    member_second_beneficiary = JobSeekerFactory(first_name="gps second beneficiary first_name")
+    member_first_beneficiary = JobSeekerFactory(
+        first_name="gps first beneficiary first_name"
+    )
+    member_second_beneficiary = JobSeekerFactory(
+        first_name="gps second beneficiary first_name"
+    )
 
-    FollowUpGroupFactory(beneficiary=member_first_beneficiary, memberships=4, memberships__member=member)
-    FollowUpGroupFactory(beneficiary=member_second_beneficiary, memberships=2, memberships__member=member)
+    FollowUpGroupFactory(
+        beneficiary=member_first_beneficiary, memberships=4, memberships__member=member
+    )
+    FollowUpGroupFactory(
+        beneficiary=member_second_beneficiary, memberships=2, memberships__member=member
+    )
 
     client.force_login(member)
 
@@ -137,7 +157,7 @@ def test_navigation(snapshot, client):
 
 
 def test_access_as_jobseeker(client):
-    user = JobSeekerFactory()
+    user = JobSeekerWithAddressFactory()
     client.force_login(user)
 
     response = client.get(reverse("gps:my_groups"))
@@ -161,7 +181,7 @@ def test_access_gps_enabled(client, snapshot):
     response = client.get(reverse("dashboard:index"))
     assert str(parse_response_to_soup(response, "#gps-card")) == snapshot
 
-    user = JobSeekerFactory()
+    user = JobSeekerWithAddressFactory()
     client.force_login(user)
     response = client.get(reverse("dashboard:index"))
     assertNotContains(response, "gps-card")
